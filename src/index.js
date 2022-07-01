0223f928561b5f7caa2eee862efd5d22b0732f4e
rammfall <15bogdan17@gmail.com>
2022-07-01 10:46:49 +0300

    Check

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ export const initConnection = () => {
     user: POSTGRES_USER || 'postgres',
     host: POSTGRES_HOST || 'localhost',
     database: POSTGRES_DB || 'postgres',
-    password: POSTGRES_PASSWORD || 'postgres',
-    port: POSTGRES_PORT || 5556,
+    password: POSTGRES_PASSWORD || ' ',
+    port: POSTGRES_PORT || 5432,
   });
 
   return client;
@@ -23,7 +23,54 @@ export const createStructure = async () => {
   const client = initConnection();
   client.connect();
 
-  // Your code is here...
+  await client.query(
+    `drop table if exists books, authors, categories, users, descriptions, reviews`
+  );
+
+  await client.query(
+    `create table users (
+    id serial primary key, name varchar(30) not null,
+    date timestamp default now())`
+  );
+
+  await client.query(
+    `create table categories(
+    id serial primary key,
+    name varchar(30) not null )`
+  );
+
+  await client.query(
+    `create table authors(
+    id serial primary key,
+    name varchar(30) not null )`
+  );
+
+  await client.query(
+    `create table books(id serial primary key,
+    title varchar(30) not null,
+    userid integer not null,
+    authorid integer not null,
+    categoryid integer not null,
+    foreign key(userid) references users (id) on delete cascade,
+    foreign key(authorid) references authors (id) on delete cascade,
+    foreign key(categoryid) references categories (id) on delete cascade)`
+  );
+
+  await client.query(`create table descriptions(
+    id serial primary key,
+    description varchar(10000) not null,
+    bookid integer not null,
+    foreign key(bookid) references books (id) on delete cascade)
+    `);
+
+  await client.query(`create table reviews(
+    id serial primary key,
+    message varchar(10000) not null,
+    userid integer not null,
+    bookid integer not null,
+    foreign key(userid) references users (id) on delete cascade,
+    foreign key(bookid) references books (id) on delete cascade)
+    `);
 
   client.end();
 };
